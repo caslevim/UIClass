594c72b3fb353b4c3133ce4a5758b5f5e618270a
caslevim <caslevim@gmail.com>
2016-12-12 14:56:29 +0530

    Add files via upload

---
+++ JS-Structure.js
@@ -0,0 +1,8 @@
+  document.write("<hr>");
+  document.write("<hr>");
+  document.write("Hello, I am calling JS file from within HTML");
+  document.write("<hr>");
+	alert("Testing ALERT");
+	confirm("Are you sure to delete it?");
+	prompt("What is your name?");
+	prompt("How old are you?","24");
---
+++ JS-syntax1.js
@@ -0,0 +1,40 @@
+var x = 5;
+		var y = 6;
+		document.getElementById("demo").innerHTML = x + y;
+
+
+		document.getElementById("demo").innerHTML = (5 + 6) * 10;
+
+
+		document.getElementById("demo").innerHTML = "Osman" + " "  + "Khan";
+
+
+		var x = 5 + 6;
+		var y = x * 10;
+		document.getElementById("demo").innerHTML = y;
+
+
+
+
+			// var x = 6; I will not be executed
+			/* var x = 6; I will not be executed
+			var x = 6; I will not be executed
+			*/
+
+		var x = 5;      // Declare x, give it the value of 5
+		var y = x + 2;  // Declare y, give it the value of x + 2 
+		document.getElementById("demo").innerHTML = y; // Write y to demo
+
+
+
+
+		var lastName = "Doe";
+		var lastname = "Peterson";
+		document.getElementById("demo").innerHTML = lastName;
+
+
+		a = 1;
+		b = 2;
+		c = a + b;
+		document.getElementById("demo1").innerHTML = c;
+
